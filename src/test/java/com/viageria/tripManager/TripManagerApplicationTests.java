d958b780a2d065cb57141f981b66e010d03a0ff6
Robson Costa <st.robson.costa@gmail.com>
2024-06-25 11:17:03 -0300

    commit inicial

---
+++ src/test/java/com/viageria/tripManager/TripManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viageria.tripManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
